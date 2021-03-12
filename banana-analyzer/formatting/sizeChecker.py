474591e8081029684e232850a982c6725a339ffb
Michael Malinowski <mike.m7@hotmail.com>
2021-03-11 22:37:02 -0500

    Update images and create scripts

---
+++ banana-analyzer/formatting/sizeChecker.py
@@ -0,0 +1,24 @@
+import os
+import cv2
+
+"""
+
+"""
+def belowMinSize(width, height):
+    pathToImages = "../../images"
+    imageFolders = os.listdir("../../images")
+    count = 0
+
+    for folder in imageFolders:
+        folderPath = f"{pathToImages}/{folder}"
+        images = os.listdir(folderPath)
+        for image in images:
+            imagePath = f"{folderPath}/{image}"
+            imageData = cv2.imread(imagePath)
+            if type(imageData) != type(None):
+                w,h,c = imageData.shape
+                if w < width or h < height:
+                    print(imagePath)
+                    count += 1
+
+    print(f"There are {count} images under the size limit")
